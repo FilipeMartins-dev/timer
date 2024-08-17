f48537e6fcaafe5c27c0ddd72544650f4887989c
FilipeMartins-dev <filipe.martins.dev@gmail.com>
2024-08-17 15:32:32 -0300

    nova logica de seleção

--- script.js
+++ script.js
@@ -9,24 +9,47 @@ function init(){
     const s = date.getSeconds()
     
     if(h < 10){
-        hour.innerHTML = `<div>0${h}</div> <div class="label">horas</div>`
+        hour.innerHTML = `<div class="number">0${h}</div> <div class="label">horas</div>`
     } else{
-        hour.innerHTML = `<div>${h}</div> <div class="label">horas</div>`
+        hour.innerHTML = `<div class="number">${h}</div> <div class="label">horas</div>`
     }
 
     if(min < 10){
-        minutes.innerHTML = `<div>0${min}</div> <div class="label">minutos</div>`
+        minutes.innerHTML = `<div class="number">0${min}</div> <div class="label">minutos</div>`
     } else {
-        minutes.innerHTML = `<div>${min}</div> <div class="label">minutos</div>`
+        minutes.innerHTML = `<div class="number">${min}</div> <div class="label">minutos</div>`
     }
 
     if(s < 10) {
-        seconds.innerHTML = `<div>0${s}</div> <div class="label">segundos</div>`
+        seconds.innerHTML = `<div class="number">0${s}</div> <div class="label">segundos</div>`
     } else {
-        seconds.innerHTML = `<div>${s}</div> <div class="label">segundos</div>`
+        seconds.innerHTML = `<div class="number">${s}</div> <div class="label">segundos</div>`
     }
-    // console.clear()
-    // console.log(`${h}:${min}:${s}`)
 }
 
 setInterval(init, 500)
+const card1 = document.querySelector('#card-hour')
+const card2 = document.querySelector('#card-minutes')
+const card3 = document.querySelector('#card-seconds')
+
+card1.addEventListener('click', select)
+card2.addEventListener('click', select)
+card3.addEventListener('click', select)
+
+function select(event){
+    const target = event.target
+    const parent = target.parentElement
+    // console.log(target)
+    // console.log(target.parentElement)
+    
+    if(target.classList.contains('number')){
+        parent.classList.toggle('selected')
+        return
+    } 
+    if(target.classList.contains('label')){
+        parent.classList.toggle('selected')
+    }
+    else{
+        target.classList.toggle('selected')
+    }
+}
