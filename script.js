6cdde7c64c52d2f00cb12c62aa4bccb0a2ddc27e
FilipeMartins-dev <filipe.martins.dev@gmail.com>
2024-08-17 14:18:24 -0300

    estilizei

--- script.js
+++ script.js
@@ -9,23 +9,24 @@ function init(){
     const s = date.getSeconds()
     
     if(h < 10){
-        hour.innerHTML = `<div>0${h}</div> <di>horas</div>`
+        hour.innerHTML = `<div>0${h}</div> <div class="label">horas</div>`
     } else{
-        hour.innerHTML = `<div>${h}</div> <div>horas</div>`
+        hour.innerHTML = `<div>${h}</div> <div class="label">horas</div>`
     }
 
     if(min < 10){
-        minutes.innerHTML = `<div>0${min}</div> <div>minutos</div>`
+        minutes.innerHTML = `<div>0${min}</div> <div class="label">minutos</div>`
     } else {
-        minutes.innerHTML = `<div>${min}</div> <di>minutos</div>`
+        minutes.innerHTML = `<div>${min}</div> <div class="label">minutos</div>`
     }
 
     if(s < 10) {
-        seconds.innerHTML = `<div>0${s}</div> <div>segundos</div>`
+        seconds.innerHTML = `<div>0${s}</div> <div class="label">segundos</div>`
     } else {
-        seconds.innerHTML = `<div>${s}</div> <div>segundos</div>`
+        seconds.innerHTML = `<div>${s}</div> <div class="label">segundos</div>`
     }
-
+    // console.clear()
+    // console.log(`${h}:${min}:${s}`)
 }
 
 setInterval(init, 500)
