8f87449651e4d2e8c02726c81de64a5981db41e5
FilipeMartins-dev <filipe.martins.dev@gmail.com>
2024-08-19 11:15:32 -0300

    criando aviso de mudança

--- script.js
+++ script.js
@@ -13,45 +13,67 @@ function init(){
     } else{
         hour.innerHTML = `<div class="number">${h}</div> <div class="label">horas</div>`
     }
-
     if(min < 10){
         minutes.innerHTML = `<div class="number">0${min}</div> <div class="label">minutos</div>`
     } else {
         minutes.innerHTML = `<div class="number">${min}</div> <div class="label">minutos</div>`
     }
-
     if(s < 10) {
         seconds.innerHTML = `<div class="number">0${s}</div> <div class="label">segundos</div>`
     } else {
         seconds.innerHTML = `<div class="number">${s}</div> <div class="label">segundos</div>`
     }
+    if(s == 0){
+        warningS()
+    }
+    if(min == 0){
+        warningH()
+    }
 }
 
-setInterval(init, 500)
-const card1 = document.querySelector('#card-hour')
-const card2 = document.querySelector('#card-minutes')
-const card3 = document.querySelector('#card-seconds')
+function warningS(){
+    console.log("OK")
+    seconds.classList.add("selected")
+    minutes.classList.add("selected")
+    setTimeout(()=>{
+        seconds.classList.remove("selected")
+        minutes.classList.remove("selected")
+    }, 1000)
+}
+function warningH(){
+    console.log("OK")
+    hour.classList.add("selected")
+    setTimeout(()=>{
+        hour.classList.remove("selected")
+    }, 1000)
+}
 
-card1.addEventListener('click', select)
-card2.addEventListener('click', select)
-card3.addEventListener('click', select)
+setInterval(init, 1000)
 
-function select(event){
-    const target = event.currentTarget
-    target.classList.toggle('selected')
+// const card1 = document.querySelector('#card-hour')
+// const card2 = document.querySelector('#card-minutes')
+// const card3 = document.querySelector('#card-seconds')
 
-    // const parent = target.parentElement
-    // console.log(target)
-    // console.log(target.parentElement)
+// card1.addEventListener('click', select)
+// card2.addEventListener('click', select)
+// card3.addEventListener('click', select)
+
+// function select(event){
+//     const target = event.currentTarget
+//     target.classList.toggle('selected')
+
+//     // const parent = target.parentElement
+//     // console.log(target)
+//     // console.log(target.parentElement)
     
-    // if(target.classList.contains('number')){
-    //     parent.classList.toggle('selected')
-    //     return
-    // } 
-    // if(target.classList.contains('label')){
-    //     parent.classList.toggle('selected')
-    // }
-    // else{
-    //     target.classList.toggle('selected')
-    // }
-}
+//     // if(target.classList.contains('number')){
+//     //     parent.classList.toggle('selected')
+//     //     return
+//     // } 
+//     // if(target.classList.contains('label')){
+//     //     parent.classList.toggle('selected')
+//     // }
+//     // else{
+//     //     target.classList.toggle('selected')
+//     // }
+// }
